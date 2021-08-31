bf2592355b332f547b4c2536f58ff7ad5572ebe1
Monday <sobaata@gmail.com>
2021-08-31 03:51:36 -0400

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+def my_method(param)
+    puts "Running my_method"
+    param + 1
+  end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (x,y)
+    x + y
+end
+
+def halve (z)
+        if z.class != Integer
+        return nil
+            end
+    z/2
+    end
